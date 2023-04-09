3b22196c5407c18d302a2a4698040c55bad531bd
Ognjen Jovanović <130289717+gazdaogi@users.noreply.github.com>
2023-04-09 16:14:08 +0200

    Add files via upload

---
+++ ZadatakTemperatura/ZadatakTemperatura/Form1.Designer.cs
@@ -0,0 +1,153 @@
+﻿namespace ZadatakTemperatura
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(167, 25);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(141, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Otvori datoteku";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(167, 73);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(141, 57);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Procescna temperatura";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(167, 150);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(141, 23);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Najtopliji dan";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(167, 199);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(141, 23);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "U redu";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // listView1
+            // 
+            this.listView1.Location = new System.Drawing.Point(12, 25);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(121, 197);
+            this.listView1.TabIndex = 4;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(346, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(155, 17);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Datoteka nije otvorena.";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(349, 87);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(249, 17);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Prosecna temperatura nije izracunata.";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(349, 150);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(181, 17);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Najtopliji dan nije izracunat.";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(769, 327);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ ZadatakTemperatura/ZadatakTemperatura/Form1.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.Security;
+
+namespace ZadatakTemperatura
+{
+    public partial class Form1 : Form
+    {
+        private String[] temperatures;
+
+        public Form1()
+        {
+            InitializeComponent();
+            button1.Click += new EventHandler(SelectButton_Click);
+        }
+
+        //open file dialog
+        private void SelectButton_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    var sr = new StreamReader(openFileDialog1.FileName);
+                    String text = sr.ReadToEnd();
+                    this.temperatures = text.Split(',');
+                    foreach (String temperature in temperatures)
+                    {
+                        listView1.Items.Add(temperature);
+                    }
+                    label1.Text = "Datoteka " + openFileDialog1.FileName;
+                }
+                catch (SecurityException ex)
+                {
+                    MessageBox.Show("Security error.\n\nError message: {ex.Message}\n\n" +
+                    "Details:\n\n{ex.StackTrace}");
+                }
+            }
+        }
+
+        //najtopliji dan
+        private void button3_Click(object sender, EventArgs e)
+        {
+            int indexOfHottestDay = 0;
+            double degreesOfHottestDay = Convert.ToDouble(this.temperatures[0]);
+
+            for (int i = 1; i < this.temperatures.Length; i++)
+            {
+                double degrees = Convert.ToDouble(this.temperatures[i]);
+                if (degrees > degreesOfHottestDay)
+                {
+                    indexOfHottestDay = i;
+                    degreesOfHottestDay = degrees;
+                }
+            }
+
+            ++indexOfHottestDay; //zbog korisnika, da ne krece od nule vec od 1
+
+            label3.Text = "Najtopliji je " + indexOfHottestDay + ". dan sa " + degreesOfHottestDay + " stepeni";
+        }
+
+        //prosecna temperature
+        private void button2_Click(object sender, EventArgs e)
+        {
+            double sumOfTemperatures = 0;
+            int countOfTemperatures = this.temperatures.Length;
+            double averageTemperature = 0.0;
+
+            foreach (String temperature in temperatures)
+            {
+                sumOfTemperatures += Convert.ToDouble(temperature);
+            }
+
+            averageTemperature = (double) sumOfTemperatures / countOfTemperatures;
+            label2.Text = "Prosecno je " + averageTemperature + " stepena";
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            System.Windows.Forms.Application.Exit();
+        }
+
+        
+    }
+}
